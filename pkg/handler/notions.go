7edf30986ddc1a14be220862c07e504636f913ca
Daria Taktashova <taktashovadasha@yandex.ru>
2022-11-26 15:53:55 +0300

    commitqwe

---
+++ pkg/handler/notions.go
@@ -0,0 +1,75 @@
+package handler
+
+import (
+	"fmt"
+	webapi "github.com/4from5/TimeHack-webapi"
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"strconv"
+)
+
+type getAllNotionsData struct {
+	Data []webapi.Notion `json:"data"`
+}
+
+func (h *Handler) getNotions(c *gin.Context) {
+	userId, err := getUserId(c)
+	if err != nil {
+		newErrorResponse(c, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	notions, err := h.services.Notions.GetAll(userId)
+	if err != nil {
+		newErrorResponse(c, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	c.JSON(http.StatusOK, getAllNotionsData{notions})
+}
+
+func (h *Handler) getNotionById(c *gin.Context) {
+	userId, err := getUserId(c)
+	if err != nil {
+		newErrorResponse(c, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		newErrorResponse(c, http.StatusBadRequest, "invalid id param")
+		return
+	}
+
+	notion, err := h.services.Notions.GetById(userId, id)
+	if err != nil {
+		newErrorResponse(c, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	c.JSON(http.StatusOK, notion)
+}
+
+func (h *Handler) createNotion(c *gin.Context) {
+	userId, err := getUserId(c)
+	if err != nil {
+		newErrorResponse(c, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	var input webapi.Notion
+	if err := c.BindJSON(&input); err != nil {
+		newErrorResponse(c, http.StatusBadRequest, err.Error())
+		return
+	}
+	fmt.Println("handler.createNotion:", input)
+	id, err := h.services.Notions.Create(userId, input)
+	if err != nil {
+		newErrorResponse(c, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	c.JSON(http.StatusOK, map[string]interface{}{
+		"notion_id": id,
+	})
+}
